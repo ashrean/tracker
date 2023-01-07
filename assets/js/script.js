43317e9c3cd487436ceca3c717357c5ddceedd88
Becka McNally <beckamcnally@gmail.com>
2023-01-06 18:58:38 -0800

    html layout header card modal and table

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,7 @@ setInterval(function (){
     console.log(time)
     }, 1000) 
    
+// not sure why this is not working https://jqueryui.com/datepicker/#default I think I followed it?
+$( function() {
+    $( "#datepicker" ).datepicker();
+} );
